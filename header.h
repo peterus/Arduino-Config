157042eec10d222fad7675c67a9bae213109f3be
Peter Buchegger <peter@pbuchegger.at>
2023-05-30 21:47:24 +0200

    update examples

--- header.h
+++ header.h
@@ -3,108 +3,28 @@
 
 class Configuration {
 public:
-class callsign {
-public:
-    
-    callsign() {}
-    callsign;
-
-};
-class network {
-public:
-    class DHCP {
-public:
-    
-    DHCP() {}
-    DHCP;
-
-};
-class static {
-public:
-    class ip {
-public:
-    
-    ip() {}
-    ip;
-
-};
-class subnet {
-public:
-    
-    subnet() {}
-    subnet;
-
-};
-class gateway {
-public:
-    
-    gateway() {}
-    gateway;
-
-};
-class dns1 {
-public:
-    
-    dns1() {}
-    dns1;
-
-};
-class dns2 {
-public:
-    
-    dns2() {}
-    dns2;
-
-};
-
-    static() {}
-    
-};
-class hostname {
-public:
-    class overwrite {
-public:
-    
-    overwrite() {}
-    overwrite;
-
-};
-class name {
-public:
-    
-    name() {}
-    name;
-
-};
-
-    hostname() {}
-    
-};
-
-    network() {}
-    
-};
-class wifi {
-public:
-    class active {
-public:
-    
-    active() {}
-    active;
-
-};
-class AP {
-public:
-    
-    AP() {}
-    AP;
-
-};
-
-    wifi() {}
-    
-};
-
+    String callsign;
+    class Network {
+    public:
+        bool DHCP;
+        class Static {
+        public:
+            IPAddress ip;
+            IPAddress subnet;
+            IPAddress gateway;
+            IPAddress dns1;
+            IPAddress dns2;
+        };
+        class Hostname {
+        public:
+            bool overwrite;
+            String name;
+        };
+    };
+    class Wifi {
+    public:
+        bool active;
+    };
 };
 
 #endif
